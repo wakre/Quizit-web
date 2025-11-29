f1fe42806101e24b2f4c90f78afbca74102dbcd0
saabd6875 <saabd6875@oslomet.no>
2025-11-29 20:50:37 +0100

    under arbeid av frontend stilen til homepage og quizlist

--- QuizitFront/src/App.tsx
+++ QuizitFront/src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Container from 'react-bootstrap/Container'
 import Homepage from './home/HomePage';
 import QuizList from './quizComponents/QuizList';
@@ -9,21 +9,48 @@ import NavMenu from './shared/NavMenu';
 import Footer from './shared/Footer';
 import './App.css'
 
+const AppContent: React.FC =() => {
+    const location = useLocation();
+  useEffect(() => {
+    if (location.pathname === '/') {
+      document.body.classList.add('homepage-bg');
+    } else {
+      document.body.classList.remove('homepage-bg');
+    }
+  }, [location.pathname]);
+  return (
+    <>
+      <NavMenu />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/QuizList" element={<Container className="my-4"><QuizList /></Container>} />
+        <Route path="/CreateQuiz" element={<Container className="my-4"><CreateQuiz /></Container>} />
+      </Routes>
+      <Footer />
+    </>
+  );
+};
 const App: React.FC = () => {
   return (
     <Router>
-      <NavMenu />
-      <Container className="my-4"> 
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/QuizList" element={<QuizList />} />
-          <Route path="/createQuiz" element={<CreateQuiz />} />
-        </Routes>
-      </Container>
-      <Footer />
+      <AppContent />
     </Router>
+  );
+};
+export default App;
+/*
+const App: React.FC = () => {
+  return (
+    <Router>
+         <NavMenu />
+         <Routes>
+           <Route path="/" element={<Homepage />} />  {/* No Container here for full-screen background *//*}
+           <Route path="/QuizList" element={<Container className="my-4"><QuizList /></Container>} />
+           <Route path="/CreateQuiz" element={<Container className="my-4"><CreateQuiz /></Container>} />
+         </Routes>
+         <Footer />
+       </Router>
     
   );
 };
-
-export default App;
+export default App;*/
--- QuizitFront/src/home/HomePage.tsx
+++ QuizitFront/src/home/HomePage.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../css/HomePage.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Homepage: React.FC = () => {
   return (
-    <>
-    <div className="text-center" style={{ marginTop: '100px' }}>
-      <h1 className="display-4">Welcome to QUIZIT</h1>
+    <div className="homepage quiz-main">
+      <h1 className='display4'>Welcome to QUIZIT</h1>
       <p className="lead">Choose a mode to get started</p>
 
       <div className="row justify-content-center" style={{ marginTop: '50px' }}>
-        <div className="col-sm-6">
+        <div className="col-sm-10">
           <div className="row">
             <div className="col-6">
               <Link
                 to="/QuizList"
-                className="btn btn-success btn-lg w-100"
+                className="btn btn-success btn-lg w-100 homepage-btn"
                 style={{ height: '100px', paddingTop: '30px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
               >
                 View Available Quizzes
@@ -24,7 +25,7 @@ const Homepage: React.FC = () => {
             <div className="col-6">
               <Link
                 to="/CreateQuiz"
-                className="btn btn-primary btn-lg w-100"
+                className="btn btn-primary btn-lg w-100 homepage-btn"
                 style={{ height: '100px', paddingTop: '30px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
               >
                 Create Your Own Quiz
@@ -35,7 +36,6 @@ const Homepage: React.FC = () => {
       </div>
     </div>
     
-    </>
   );
 };
 
